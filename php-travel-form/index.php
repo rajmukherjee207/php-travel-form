e59eda47844ce2a3fc3dbcc02f5a60484e4f33ee
Raj mukherjee <rajmukherjee207@gmail.com>
2025-03-31 13:41:33 +0530

    Update index.php

--- php-travel-form/index.php
+++ php-travel-form/index.php
@@ -1,3 +1,21 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+
+// Create a connection
+$con = mysqli_connect($server, $username, $password);
+
+// Check connection
+if(!$con){
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Success connecting to the DB";
+}
+?>
+
+
+/*
 $server = "localhost";
 
 $username = "root";
@@ -5,4 +23,4 @@ $username = "root";
 $password = "";
 
 $con = mysqli_connect($server, $username, $password);
-if(!$con){echo "Success connecting to the db";}
+if(!$con){echo "Success connecting to the db";} */
